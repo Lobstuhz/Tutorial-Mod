5d6cb4d589199a16af7103b10fb0a96910352ab6
Jack <97704578+Lobstuhz@users.noreply.github.com>
2024-12-19 01:20:36 -0600

    First custom item!

--- src/main/java/net/jevens/tutorialmod/TutorialMod.java
+++ src/main/java/net/jevens/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.jevens.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jevens.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -22,6 +24,8 @@ public class TutorialMod {
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -34,7 +38,9 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.XYLITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
